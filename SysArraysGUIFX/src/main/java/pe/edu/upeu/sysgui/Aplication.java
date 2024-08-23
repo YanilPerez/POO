2eba7613bc7d750cccbd45922920f7203557a234
YanilPerez <60850787@upeu.edu.pe>
2024-08-23 11:13:29 -0500

    Add files via upload

---
+++ SysArraysGUIFX/src/main/java/pe/edu/upeu/sysgui/Aplication.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.sysgui;
+
+public class Aplication {
+    public static void main(String[] args) {
+        SysArraysGuifxApplication.main(args);
+    }
+}
+
---
+++ SysArraysGUIFX/src/main/java/pe/edu/upeu/sysgui/SysArraysGuifxApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.sysgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysArraysGuifxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysArraysGuifxApplication.class, args);
+	}
+
+}
