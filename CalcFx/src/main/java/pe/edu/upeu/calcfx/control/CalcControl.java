9636a20540eb05e232ac79242b58fd286bc766ed
YanilPerez <60850787@upeu.edu.pe>
2024-09-08 01:40:30 -0500

    Add files via upload

--- CalcFx/src/main/java/pe/edu/upeu/calcfx/control/CalcControl.java
+++ CalcFx/src/main/java/pe/edu/upeu/calcfx/control/CalcControl.java
@@ -19,30 +19,41 @@ public class CalcControl {
         case"btn7","btn8","btn9","btn4","btn5","btn6","btn1","btn2","btn3","btn0":{
             escribirNumeros(button.getText());
         }break;
-        case "btnSum","btnDiv","btnRes","btnMul":{
+        case "btnSum","btnDiv","btnRes","btnMul","btnRai":{
             operador(button.getText());
         } break;
         case "btnIgual":{
-
+            calcularResultado();
         }break;
         case "btnBorrar":{
             txtResultado.clear();
         }
     }
 }
-public void operador(String event){
-    txtResultado.appendText(" "+valor+" ");
+public void operador(String valor){
+    if (valor.equals("√")) {
+        txtResultado.appendText(" " + valor);
+    } else {
+        txtResultado.appendText(" " + valor + " ");
+    }
         }
 public void calcularResultado(){
     String[]valores=txtResultado.getText().split(" ");
-
-    double val1=Double.parseDouble(String.valueOf(valores[0]));
+    double val1 = Double.parseDouble(valores[0]);
+    if(valores.length ==3){
     double val2=Double.parseDouble(String.valueOf(valores[2]));
     switch (valores[1]){
         case "+":{txtResultado.setText(String.valueOf(val1+val2));}break;
         case "-":{txtResultado.setText(String.valueOf(val1-val2));}break;
         case "/":{txtResultado.setText(String.valueOf(val1/val2));}break;
         case "*":{txtResultado.setText(String.valueOf(val1*val2));}break;
+    }
+    }
+    else if(valores.length == 2){
+        double val=Double.parseDouble(String.valueOf(valores[1]));
+        switch (valores[1]){
+            case "√":{txtResultado.setText(String.valueOf(Math.sqrt(val)));}break;
+        }
     }
 }
 public void escribirNumeros(String valor){
