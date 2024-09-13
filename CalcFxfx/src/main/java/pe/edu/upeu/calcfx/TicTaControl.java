96047956c1ae0f749e936fc89899fe32bb183621
YanilPerez <60850787@upeu.edu.pe>
2024-09-13 10:33:25 -0500

    Add files via upload

---
+++ CalcFxfx/src/main/java/pe/edu/upeu/calcfx/TicTaControl.java
@@ -0,0 +1,18 @@
+package pe.edu.upeu.calcfx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TicTaControl {
+
+    @FXML
+    public void initialize() {
+
+    }
+    @FXML
+    void accionButton(ActionEvent e){
+
+    }
+}
